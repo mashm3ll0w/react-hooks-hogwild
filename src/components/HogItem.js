d8374dce88e203490df39f81d430a385cb899309
Charles Swaleh <charlswall@live.com>
2023-01-30 21:55:34 +0300

    Completed functionality to show and hide other hog details when a hog tile is clicked

--- src/components/HogItem.js
+++ src/components/HogItem.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function HogItem({ hog }) {
+	const [showHogDetails, setShowHogDetails] = useState(false);
+
+	function handleClick(e) {
+		setShowHogDetails((showHogDetails) => !showHogDetails);
+	}
+
+	const showDetails = () => {
+		return (
+			<>
+				<div className="meta">
+					<div className="description">Specialty: {hog.specialty}</div>
+					<div className="description">Weight: {hog.weight}</div>
+					<div className="description">Greased: {hog.greased ? "Yes" : "No"}</div>
+				</div>
+				<div className="description">Highest medal achieved: {hog["highest medal achieved"]}</div>
+			</>
+		);
+	};
+
 	return (
-		<>
-			<div class="card ui eight wide column">
-				<div class="image">
-					<img src={hog.image} alt={hog.name}/>
-				</div>
-				<div class="content">
-					<div class="header">{hog.name}</div>
-					<div class="meta">
-						<p>{hog.specialty}</p>
-					</div>
-					<div class="description">{hog["highest medal achieved"]}</div>
-				</div>
+		<div className="card ui eight wide column" onClick={handleClick}>
+			<div className="image">
+				<img src={hog.image} alt={hog.name} />
 			</div>
-		</>
+			<div className="content">
+				<div className="header">{hog.name}</div>
+				{showHogDetails	? showDetails() : null}
+			</div>
+		</div>
 	);
 }
 
